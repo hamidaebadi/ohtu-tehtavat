015f195347f240a2ae3567d81ac4ef9eb9e31b23
Hamid Aebadi <hamidebadi1996@yahoo.com>
2022-03-15 15:30:23 +0200

    Tehtävässä 5, testi numero 5 tehty ja suoritettu läpi

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -26,3 +26,10 @@ class TestOstoskori(unittest.TestCase):
         self.kori.lisaa_tuote(maito)
         self.kori.lisaa_tuote(kurkku)
         self.assertEqual(self.kori.tavaroita_korissa(), 2)
+
+    def test_Kahden_eri_tuotteen_lisaamisen_jalkeen_ostoskorin_hinta_sama_kuin_tuotteiden_hintojen_summa(self):
+        maito = Tuote("Maito", 3)
+        kurkku = Tuote("Kurkku", 1)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(kurkku)
+        self.assertEqual(self.kori.hinta(), 4)

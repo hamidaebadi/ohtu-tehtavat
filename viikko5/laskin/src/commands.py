6b7d8a626590380b41d10d9e41cd589dcdbd7503
Hamid Aebadi <hamidebadi1996@yahoo.com>
2022-03-24 06:58:53 +0200

    Tehtävä 5 tehty

--- viikko5/laskin/src/commands.py
+++ viikko5/laskin/src/commands.py
@@ -3,13 +3,16 @@ class Summa:
         self.__sovellus_logiikka = sovelluslogiikka
         self.__syote = syote
 
-    def suorita(self):
+    def _get_value(self):
         arvo = 0
         try:
             arvo = int(self.__syote())
         except Exception:
             pass
-        
+        return arvo
+
+    def suorita(self):
+        arvo = self._get_value()
         self.__sovellus_logiikka.plus(arvo)
 
 class Erotus:
@@ -17,12 +20,16 @@ class Erotus:
         self.__sovellus_logiikka = sovelluslogiikka
         self.__syote = syote
 
-    def suorita(self):
+    def _get_value(self):
         arvo = 0
         try:
             arvo = int(self.__syote())
         except Exception:
             pass
+        return arvo
+
+    def suorita(self):
+        arvo = self._get_value()
         self.__sovellus_logiikka.miinus(arvo)
 
 class Nollaus:
@@ -33,7 +40,19 @@ class Nollaus:
     def suorita(self):
         self.__sovellus_logiikka.nollaa()
 
+
+    def _get_value(self):
+        arvo = 0
+        try:
+            arvo = int(self.__syote())
+        except Exception:
+            pass
+        return arvo
+
 class Kumoa:
     def __init__(self, sovelluslogiikka, syote):
         self.__sovellus_logiikka = sovelluslogiikka
         self.__syote = syote
+
+    def suorita(self):
+        self.__sovellus_logiikka.aseta_arvo(self.__sovellus_logiikka.edellinen_tulos)
